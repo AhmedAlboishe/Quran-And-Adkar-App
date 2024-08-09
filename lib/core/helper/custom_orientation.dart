7cccc9578517c449a509d734974e56b739c11b3f
Ahmed Alboishe <ahmedalboishelabtop@gmail.com>
2024-08-09 12:18:07 +0200

    CustomOrientation

---
+++ lib/core/helper/custom_orientation.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+extension CustomOrientation on BuildContext {
+  dynamic customOrientation(var n1, var n2) {
+    Orientation orientation = MediaQuery.orientationOf(this);
+    return orientation == Orientation.portrait ? n1 : n2;
+  }
+}
